3e126b9afd562e4706456c80c6020627b1d66e3f
vinhkosd <vinh.le@robustaeng.com>
2021-10-03 05:45:03 +0700

    add assets sound, weapon

---
+++ [Assets]/assets-weapons/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+data_file 'WEAPONINFO_FILE' 'meta/weapons.meta'
+data_file 'WEAPON_METADATA_FILE' 'meta/weaponarchetypes.meta'
+data_file 'WEAPON_SHOP_INFO' 'meta/shop_weapon.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'meta/weaponanimations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'meta/contentunlocks.meta'
+data_file 'LOADOUTS_FILE' 'meta/loadouts.meta'
+data_file 'PED_PERSONALITY_FILE' 'meta/pedpersonality.meta'
+
+files {
+  'meta/contentunlocks.meta',
+  'meta/loadouts.meta',
+  'meta/pedpersonality.meta',
+  'meta/shop_weapon.meta',
+  'meta/weaponanimations.meta',
+  'meta/weaponarchetypes.meta',
+  'meta/weapons.meta'
+}
